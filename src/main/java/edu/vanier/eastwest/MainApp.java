53c8015fd9ebe1f26bf25d93f818b18eade51d75
Hyujinx <66443004+Hyujinx@users.noreply.github.com>
2024-03-24 23:11:34 -0400

    remove space

--- src/main/java/edu/vanier/eastwest/MainApp.java
+++ src/main/java/edu/vanier/eastwest/MainApp.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class                         MainApp extends Application {
+public class MainApp extends Application {
 
     public static Scene scene;
 

5c9b440276292c40d436647402858e551316e2d4
exisodd <ethantran.mail@gmail.com>
2024-04-11 12:30:23 -0700

    moved logic for grid and axes to separate class

--- src/main/java/edu/vanier/eastwest/controllers/SimulatorController.java
+++ src/main/java/edu/vanier/eastwest/controllers/SimulatorController.java
@@ -5,6 +5,7 @@ import edu.vanier.eastwest.models.Body;
 import edu.vanier.eastwest.models.MySplitPaneSkin;
 import edu.vanier.eastwest.models.TreeNode;
 import edu.vanier.eastwest.models.Vector3D;
+import static edu.vanier.eastwest.util.Utility.*;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -274,8 +275,6 @@ public class SimulatorController {
         };
         MainApp.scene.setOnMouseReleased(mouseReleasedHandler);
 
-
-
         // Mouse dragging controls
         EventHandler<MouseEvent> mouseDraggedHandler = event -> {
 
@@ -375,7 +374,7 @@ public class SimulatorController {
      * Unselects all ToggleButtons within the vbox that were not clicked.
      * @param selected ToggleButton that was clicked. This ToggleButton will remain selected.
      */
-    private void toggleToolButtons(ToggleButton selected){
+    private void toggleToolButtons(ToggleButton selected) {
         for (Node node : vbTools.getChildren()){
             if(node instanceof ToggleButton){
                 if(!node.equals(selected)){
@@ -383,132 +382,6 @@ public class SimulatorController {
                 }
             }
         }
-    }
-
-    /**
-     * Generates a group containing three cylinders representing X, Y, and Z axes.
-     * Each axis is oriented along a different axis direction (X, Y, or Z) and colored accordingly.
-     * The intersection of the 3 axes represent the coordinate (0, 0, 0).
-     *
-     * @param scale The scaling factor applied to the axes
-     * @return a Group object containing the X, Y, and Z axes with specified scaling
-     */
-    public Group getAxes(double scale) {
-        Cylinder axisX = new Cylinder(1, 9999);
-        axisX.getTransforms().addAll(new Rotate(90, Rotate.Z_AXIS));
-        PhongMaterial redMat = new PhongMaterial();
-        redMat.setDiffuseColor(Color.RED);
-        axisX.setMaterial(redMat);
-
-        Cylinder axisY = new Cylinder(1, 9999);
-        axisY.setMaterial(new PhongMaterial(Color.GREEN));
-
-        Cylinder axisZ = new Cylinder(1, 9999);
-        axisZ.setMaterial(new PhongMaterial(Color.BLUE));
-        axisZ.getTransforms().addAll(new Rotate(90, Rotate.X_AXIS));
-
-        Group group = new Group(axisX, axisY, axisZ);
-        group.getTransforms().add(new Scale(scale, scale, scale));
-        return group;
-    }
-
-    /**
-     * Creates a polygon mesh based on specified width, height, and subdivision parameters.
-     *
-     * @param width   The width of the mesh
-     * @param height  The height of the mesh
-     * @param subDivX The number of subdivisions along the X-axis
-     * @param subDivY The number of subdivisions along the Y-axis
-     * @return a PolygonMesh object representing the created mesh
-     */
-    public PolygonMesh createMesh(float width, float height, int subDivX, int subDivY) {
-        final float minX = -width / 2f;
-        final float minY = -height / 2f;
-        final float maxX = width / 2f;
-        final float maxY = height / 2f;
-
-        final int pointSize = 3;
-        final int texCoordSize = 2;
-        // 4 point indices and 4 texCoord indices per face
-        final int faceSize = 8;
-        int numDivX = subDivX + 1;
-        int numVerts = (subDivY + 1) * numDivX;
-        float points[] = new float[numVerts * pointSize];
-        float texCoords[] = new float[numVerts * texCoordSize];
-        int faceCount = subDivX * subDivY;
-        int faces[][] = new int[faceCount][faceSize];
-
-        // Create points and texCoords
-        for (int y = 0; y <= subDivY; y++) {
-            float dy = (float) y / subDivY;
-            double fy = (1 - dy) * minY + dy * maxY;
-
-            for (int x = 0; x <= subDivX; x++) {
-                float dx = (float) x / subDivX;
-                double fx = (1 - dx) * minX + dx * maxX;
-
-                int index = y * numDivX * pointSize + (x * pointSize);
-                points[index] = (float) fx;
-                points[index + 1] = (float) fy;
-                points[index + 2] = 0.0f;
-
-                index = y * numDivX * texCoordSize + (x * texCoordSize);
-                texCoords[index] = dx;
-                texCoords[index + 1] = dy;
-            }
-        }
-
-        // Create faces
-        int index = 0;
-        for (int y = 0; y < subDivY; y++) {
-            for (int x = 0; x < subDivX; x++) {
-                int p00 = y * numDivX + x;
-                int p01 = p00 + 1;
-                int p10 = p00 + numDivX;
-                int p11 = p10 + 1;
-                int tc00 = y * numDivX + x;
-                int tc01 = tc00 + 1;
-                int tc10 = tc00 + numDivX;
-                int tc11 = tc10 + 1;
-
-                faces[index][0] = p00;
-                faces[index][1] = tc00;
-                faces[index][2] = p10;
-                faces[index][3] = tc10;
-                faces[index][4] = p11;
-                faces[index][5] = tc11;
-                faces[index][6] = p01;
-                faces[index++][7] = tc01;
-            }
-        }
-
-        int[] smooth = new int[faceCount];
-
-        PolygonMesh mesh = new PolygonMesh(points, texCoords, faces);
-        mesh.getFaceSmoothingGroups().addAll(smooth);
-        return mesh;
-    }
-
-    /**
-     * Generates a group containing grid lines in the XY, XZ, and YZ planes, with specified size and spacing.
-     *
-     * @param size  The size of the grid in the X and Y directions
-     * @param delta The spacing between grid lines
-     * @return a Group object containing grid lines in the XZ plane and its subgrid
-     */
-    public PolygonMeshView getGrid(float size, float delta) {
-        if (delta < 1) {
-            delta = 1;
-        }
-        final PolygonMesh plane = createMesh(size, size, (int) (size / delta), (int) (size / delta));
-
-        PolygonMeshView meshViewXZ = new PolygonMeshView(plane);
-        meshViewXZ.setDrawMode(DrawMode.LINE);
-        meshViewXZ.setCullFace(CullFace.NONE);
-        meshViewXZ.getTransforms().add(new Rotate(90, Rotate.X_AXIS));
-
-        // Only render the XZ plane
-        return meshViewXZ;
     }
 
     public List<Body> bodies() {
---
+++ src/main/java/edu/vanier/eastwest/util/Utility.java
@@ -0,0 +1,142 @@
+package edu.vanier.eastwest.util;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.CullFace;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
+import org.fxyz3d.shapes.polygon.PolygonMesh;
+import org.fxyz3d.shapes.polygon.PolygonMeshView;
+
+public class Utility {
+
+    /**
+     * Generates a group containing three cylinders representing X, Y, and Z axes.
+     * Each axis is oriented along a different axis direction (X, Y, or Z) and colored accordingly.
+     * The intersection of the 3 axes represent the coordinate (0, 0, 0).
+     *
+     * @param scale The scaling factor applied to the axes
+     * @return a Group object containing the X, Y, and Z axes with specified scaling
+     */
+    public static Group getAxes(double scale) {
+        Cylinder axisX = new Cylinder(1, 9999);
+        axisX.getTransforms().addAll(new Rotate(90, Rotate.Z_AXIS));
+        PhongMaterial redMat = new PhongMaterial();
+        redMat.setDiffuseColor(Color.RED);
+        axisX.setMaterial(redMat);
+
+        Cylinder axisY = new Cylinder(1, 9999);
+        axisY.setMaterial(new PhongMaterial(Color.GREEN));
+
+        Cylinder axisZ = new Cylinder(1, 9999);
+        axisZ.setMaterial(new PhongMaterial(Color.BLUE));
+        axisZ.getTransforms().addAll(new Rotate(90, Rotate.X_AXIS));
+
+        Group group = new Group(axisX, axisY, axisZ);
+        group.getTransforms().add(new Scale(scale, scale, scale));
+        return group;
+    }
+
+
+    /**
+     * Creates a polygon mesh based on specified width, height, and subdivision parameters.
+     *
+     * @param width   The width of the mesh
+     * @param height  The height of the mesh
+     * @param subDivX The number of subdivisions along the X-axis
+     * @param subDivY The number of subdivisions along the Y-axis
+     * @return a PolygonMesh object representing the created mesh
+     */
+    public static PolygonMesh createMesh(float width, float height, int subDivX, int subDivY) {
+        final float minX = -width / 2f;
+        final float minY = -height / 2f;
+        final float maxX = width / 2f;
+        final float maxY = height / 2f;
+
+        final int pointSize = 3;
+        final int texCoordSize = 2;
+        // 4 point indices and 4 texCoord indices per face
+        final int faceSize = 8;
+        int numDivX = subDivX + 1;
+        int numVerts = (subDivY + 1) * numDivX;
+        float points[] = new float[numVerts * pointSize];
+        float texCoords[] = new float[numVerts * texCoordSize];
+        int faceCount = subDivX * subDivY;
+        int faces[][] = new int[faceCount][faceSize];
+
+        // Create points and texCoords
+        for (int y = 0; y <= subDivY; y++) {
+            float dy = (float) y / subDivY;
+            double fy = (1 - dy) * minY + dy * maxY;
+
+            for (int x = 0; x <= subDivX; x++) {
+                float dx = (float) x / subDivX;
+                double fx = (1 - dx) * minX + dx * maxX;
+
+                int index = y * numDivX * pointSize + (x * pointSize);
+                points[index] = (float) fx;
+                points[index + 1] = (float) fy;
+                points[index + 2] = 0.0f;
+
+                index = y * numDivX * texCoordSize + (x * texCoordSize);
+                texCoords[index] = dx;
+                texCoords[index + 1] = dy;
+            }
+        }
+
+        // Create faces
+        int index = 0;
+        for (int y = 0; y < subDivY; y++) {
+            for (int x = 0; x < subDivX; x++) {
+                int p00 = y * numDivX + x;
+                int p01 = p00 + 1;
+                int p10 = p00 + numDivX;
+                int p11 = p10 + 1;
+                int tc00 = y * numDivX + x;
+                int tc01 = tc00 + 1;
+                int tc10 = tc00 + numDivX;
+                int tc11 = tc10 + 1;
+
+                faces[index][0] = p00;
+                faces[index][1] = tc00;
+                faces[index][2] = p10;
+                faces[index][3] = tc10;
+                faces[index][4] = p11;
+                faces[index][5] = tc11;
+                faces[index][6] = p01;
+                faces[index++][7] = tc01;
+            }
+        }
+
+        int[] smooth = new int[faceCount];
+
+        PolygonMesh mesh = new PolygonMesh(points, texCoords, faces);
+        mesh.getFaceSmoothingGroups().addAll(smooth);
+        return mesh;
+    }
+
+    /**
+     * Generates a group containing grid lines in the XY, XZ, and YZ planes, with specified size and spacing.
+     *
+     * @param size  The size of the grid in the X and Y directions
+     * @param delta The spacing between grid lines
+     * @return a Group object containing grid lines in the XZ plane and its subgrid
+     */
+    public static PolygonMeshView getGrid(float size, float delta) {
+        if (delta < 1) {
+            delta = 1;
+        }
+        final PolygonMesh plane = createMesh(size, size, (int) (size / delta), (int) (size / delta));
+
+        PolygonMeshView meshViewXZ = new PolygonMeshView(plane);
+        meshViewXZ.setDrawMode(DrawMode.LINE);
+        meshViewXZ.setCullFace(CullFace.NONE);
+        meshViewXZ.getTransforms().add(new Rotate(90, Rotate.X_AXIS));
+
+        // Only render the XZ plane
+        return meshViewXZ;
+    }
+}

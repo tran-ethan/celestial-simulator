5f67ba80a1d621c6425ce7d989a4e652ae228025
Hyujinx <66443004+Hyujinx@users.noreply.github.com>
2024-05-04 01:31:25 -0400

    Update TestSimulatorController.java

--- src/test/java/TestSimulatorController.java
+++ src/test/java/TestSimulatorController.java
@@ -20,7 +20,7 @@ public class TestSimulatorController {
     public void TestGetGravityVerifyZeroPoint3D(){
         Point3D p1 = new Point3D(1,0,1);
         Point3D p2 = new Point3D(0,0,0);
-        assertEquals(new Point3D(-0.125,0,-0.125), controller.getGravity(p1,p2,1,1,1));
+        assertEquals(new Point3D(-0.0125,0,-0.0125), controller.getGravity(p1,p2,1,1,1));
         assertEquals(new Point3D(0,0,0), controller.getGravity(p2,p2,1,1,1));
     }
 
@@ -50,10 +50,10 @@ public class TestSimulatorController {
         Point3D p1 = new Point3D(1, 0,1);
         Point3D p2 = new Point3D(-242,0,-300);
         Point3D p3 = new Point3D(422, 32, 23);
-        assertEquals(new Point3D(-0.0020987523535689447,0,-0.002599689129317911), controller.getGravity(p1,p2,500,1,1));
-        assertEquals(new Point3D(-0.9137116400873898,-0.04403429590782602,-0.44447117431961886), controller.getGravity(p3,p2,555555,434,5));
-        assertEquals(new Point3D(0.000005042249875685097,0.0000003832588979143066,0.00000026349049231608577), controller.getGravity(p1,p3,34134,35,14143));
-        assertEquals(new Point3D(-0.000014857223122706321,0,-0.000018403391604669146), controller.getGravity(p1,p2,222,1414,123));
+        assertEquals(new Point3D(-0.00020987523535689447,0,-0.0002599689129317911), controller.getGravity(p1,p2,500,1,1));
+        assertEquals(new Point3D(-0.09137116400873898,-0.004403429590782602,-0.04444711743196189), controller.getGravity(p3,p2,555555,434,5));
+        assertEquals(new Point3D(0.0000005042249875685097,0.000000038325889791430663,0.000000026349049231608582), controller.getGravity(p1,p3,34134,35,14143));
+        assertEquals(new Point3D(-0.0000014857223122706321,0,-0.0000018403391604669148), controller.getGravity(p1,p2,222,1414,123));
     }
 
 }
